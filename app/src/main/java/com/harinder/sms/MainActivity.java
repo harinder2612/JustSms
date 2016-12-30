4b62729c4399cb516fd4f8031045e2766e798854
Harinder Pal <Harinder Pal>
2016-12-31 03:17:06 +0530

    Initial commit

---
+++ app/src/main/java/com/harinder/sms/MainActivity.java
@@ -0,0 +1,58 @@
+package com.harinder.sms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    SmsManager sm;
+    EditText e1,e2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = (EditText) findViewById(R.id.editText);
+        e2 = (EditText) findViewById(R.id.editText2);
+    }
+
+
+    public void send(View v)
+    {
+        try {
+            sm = SmsManager.getDefault();
+            sm.sendTextMessage(e1.getText().toString(),null,e2.getText().toString(), null, null);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
